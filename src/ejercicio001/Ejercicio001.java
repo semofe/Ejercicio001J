e594aaee65435957ab1d06518b520535692926ee
Sergio Morillo <sergiofabrizio12@gmail.com>
2025-04-14 14:15:18 -0500

    Ejercicio01

---
+++ src/ejercicio001/Ejercicio001.java
@@ -0,0 +1,20 @@
+package ejercicio001;
+
+import java.util.Scanner;
+
+public class Ejercicio001 {
+
+    public static void main(String[] args) {
+        
+        Poke p1 = new Poke();
+        
+        p1.name = "Charizard";
+        p1.type = "Fire" ;
+
+        
+        System.out.println("Name: " + p1.name + " - " + "Type: "+ p1.type + " - " + "Level: " + p1.lvl);
+        
+        
+    }
+    
+}
---
+++ src/ejercicio001/Poke.java
@@ -0,0 +1,13 @@
+
+package ejercicio001;
+
+public class Poke{    
+    String name;
+    String type;
+    int lvl;
+    
+    Poke(){
+        lvl=23;
+    }
+    
+}
